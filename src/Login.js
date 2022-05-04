f3e1a035a570a1255c4f2c5ea66a83fa1bb6fde8
shivangi234 <sahu.sony23@gmail.com>
2022-05-04 10:56:56 +0530

    done

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,12 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
+import React, { useEffect, useState } from "react";
+import "./App.css";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 import LockIcon from "@mui/icons-material/Lock";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
@@ -12,146 +15,219 @@ import LoginIcon from "@mui/icons-material/Login";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import Cryptojs from "crypto-js";
 import back1 from "../src/Images/back1.jpg";
 
+import { useNavigate } from "react-router-dom";
 
 const theme = createTheme();
 
+toast.configure();
+export default function App() {
+  let navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState(" ");
+  const [organization, setOrganization] = useState("");
+  const [organizations, setOrganizations] = useState([]);
+  const [proceed, setProceed] = useState(false);
 
-export default function Login() {
-  const notify = () => toast("Login Sucessfully !");
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    const requestData = {
+      user_name: username,
+      password: password,
+      org_code: organization,
+    };
+
+
+    axios({
+      url: "https://liveexam.edusols.com/api/tassess_api.php?oper=LOGIN_CHECK",
+      method: "POST",
+      data: requestData,
+    }).then((response) => {
+      const result = response.data;
+      if (result.status === 200) {
+        if (result.status_message === "Item_Found") {
+          toast.success("Login Sucessfully !");
+
+          if (result.data.password_change_status === "YES")
+            setTimeout(() => {
+              navigate("/photovalidation");
+            }, 3000);
+          else
+            setTimeout(() => {
+              navigate("/password");
+            }, 3000);
+        } else {
+          setTimeout(() => {
+            toast.error("Login Failed !");
+          }, 3000);
+        }
+      } else {
+        alert("Something went wrong");
+      }
+    });
+
+    if (username === "" || password === "" || organization === "") {
+      toast.error("User Name Required!");
+      toast.warn("User Password Required!");
+      toast.error("Organizations Required!");
+    } else {
+      setTimeout(() => {
+        setProceed(true);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://liveexam.edusols.com/api/tassess_api.php?oper=ORG_LIST")
+      .then((response) => {
+        const org = response.data;
+        setOrganizations(org.data);
+      });
+  }, []);
+
+  const organizationList = organizations.map((item) => (
+    <MenuItem key={item.org_code} value={item.org_code}>
+      {item.org_name}
+    </MenuItem>
+  ));
 
   return (
-    <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
+    <form>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Grid
-          item
-          xs={12}
-          sx={{
-            backgroundImage: `url(${back1})`,
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
+        <Grid container spacing={2}>
           <Grid
             item
-            xs={6}
-            sm={8}
-            md={5}
-            ml={44}
-            mt={10}
-            component={Paper}
-            elevation={6}
-            
+            lg={12}
+            md={6}
+            sm={3}
+            sx={{
+              backgroundImage:
+              `url(${back1})`,
+              backgroundRepeat: "no-repeat",
+              maxHeight: "100vh",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+            style={{ minHeight: "100vh" }}
           >
-            <Container maxWidth="sm" style={{ m:2 }}>
-              <Box
-                component="form"
-                sx={{
-                  "& > :not(style)": { m: 1 },
+            <Grid container justifyContent="center">
+              <Grid item lg={6} md={3} sm={2} mt={8} component={Paper}>
+                <Container maxWidth="sm" style={{ m: 2 }}>
+                  <Box
+                    sx={{
+                      "& > :not(style)": { m: 1 },
+                    }}
+                    noValidate
+                  >
+                    <Typography
+                      variant="h5"
+                      component="div"
+                      sx={{ flexGrow: 1, mb: 2 }}
+                    >
+                      <LockIcon color="primary" />
+                      <b>Login</b>
+                    </Typography>
+                    <TextField
+                      label="User Name"
+                      variant="outlined"
+                      type="text"
+                      name="username"
+                      id="username"
+                      onChange={(e) => setUsername(e.target.value)}
+                      fullWidth
+                    />
+                    <TextField
+                      autoComplete="password"
+                      label="Password"
+                      variant="outlined"
+                      type="password"
+                      name="password"
+                      id="password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      fullWidth
+                    />
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">
+                        Organizations
+                      </InputLabel>
 
-                  
-                }}
-                noValidate
-                
-              >
-                <LockIcon color="primary" />
-                <Typography
-                  variant="h5"
-                  component="div"
-                  sx={{ flexGrow: 1, mb: 2 }}
-                >
-                  <b>Login</b>
-                </Typography>
-                <TextField
-                  
-                  label="User Name"
-                  variant="outlined"
-                  type="text"
-                  name="User Name"
-                  fullWidth
-                />
-                <TextField
-                  autoComplete="password"
-                  label="Password"
-                  variant="outlined"
-                  type="password"
-                  fullWidth
-                />
-                <Autocomplete
-                  disablePortal
-                  id="combo-box-demo"
-                 
-                  fullWidth
-                  options={top5Orgs}
-                  renderInput={(params) => (
-                    <TextField {...params} label="Organisations" />
-                  )}
-                />
-                <Button
-                  variant="contained"
-                  size="medium"
-                  color="success"
-                  endIcon={<LoginIcon />}
-                  fullWidth
-                  mt="2" 
-                  onClick={notify}
-                  
-                >
-               <span style={{textAlign:"center"}}> Proceed <ToastContainer style={{ml:12}} /></span> 
-                
-                </Button>
-              </Box>
-              
-            </Container>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        label="Organization"
+                        name="organization"
+                        fullWidth
+                        onChange={(e) => setOrganization(e.target.value)}
+                      >
+                        {organizationList}
+                      </Select>
+                    </FormControl>
 
+                    {proceed ? (
+                      <Button
+                        variant="contained"
+                        size="medium"
+                        color="success"
+                        fullWidth
+                        mt="2"
+                        disabled
+                      >
+                        Processing....
+                        <div class="fa fa-spinner fa-spin" />
+                      </Button>
+                    ) : (
+                      <Button
+                        variant="contained"
+                        size="medium"
+                        color="success"
+                        endIcon={<LoginIcon />}
+                        fullWidth
+                        mt="2"
+                        onClick={submitForm}
+                      >
+                        <ToastContainer />
+                        Proceed
+                      </Button>
+                    )}
+                  </Box>
+                </Container>
+              </Grid>
+            </Grid>
+            <Box sx={{ mt: 12 }}>
+              <Copyright sx={{ mt: 5, color: "white" }} />
+              <Version sx={{ mb: 8, color: "white" }} />
+            </Box>
           </Grid>
-          
-          <Copyright  sx={{ mt: 5,color:'white' }}/>
-          <Version  sx={{ mb: 8,color:'white'  }}/>
-         
-          
         </Grid>
-       
-      </Grid>
-      
-    </ThemeProvider>
+      </ThemeProvider>
+    </form>
   );
 }
 
-const top5Orgs = [
-  { label: "CENTURION UNIVERSITY" },
-  { label: "UTKAL UNIVERSITY" },
-  { label: "RD UNIVERSITY" },
-  { label: "FM UNIVERSITY" },
-  { label: "REVENSHAW UNIVERSITY" },
-];
-
 function Copyright(props) {
-    return (
-      <Typography style={{}}
-      variant="body2" color="text.secondary" ml={5} 
-      {...props}>
-        {"Copyright ©  STL 2021-22 All Rights Reserved"}
-      </Typography>
-      
-    );
-  }
-  function Version (props) {
-    return (
-      <Typography
-      variant="body2" color="text.secondary" mr={6} align="right"
-      {...props}>
-        {"Current Version 2.3"}
-      </Typography>
-      
-    );
-  }
+  return (
+    <Typography variant="body2" color="text.secondary" ml={5} {...props}>
+      {"Copyright ©  STL 2021-22 All Rights Reserved"}
+    </Typography>
+  );
+}
+function Version(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      mr={6}
+      align="right"
+      {...props}
+    >
+      {"Current Version 2.3"}
+    </Typography>
+  );
+}
--- src/Password.js
+++ src/Password.js
@@ -4,39 +4,94 @@ import axios from "axios";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import "./App.css";
-
-import { Button } from "@mui/material";
+import passwordValidator from "password-validator";
+import ReportIcon from "@mui/icons-material/Report";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 import EditIcon from "@mui/icons-material/Edit";
-import LogoutIcon from "@mui/icons-material/Logout";
+import back1 from "../src/Images/back1.jpg";
 import ChangeCircleIcon from "@mui/icons-material/ChangeCircle";
-import { ToastContainer, toast } from "react-toastify";
+import { Button } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
+
 import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import warning from "../src/Images/warning.gif";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
 export default function App() {
+  var schema = new passwordValidator();
   const [password, setPassword] = useState(" ");
   const [newpassword, setNewpassword] = useState("");
   const [confirmnewpassword, setConfirmnewpassword] = useState(" ");
+  const [ma, setMa] = useState(false);
+  const [sl, setSl] = useState(false);
+  const [Na, setNa] = useState(false);
+  const [Sc, setSc] = useState(false);
+  const [length, setLength] = useState(false);
   const [validate, setValidate] = useState(true);
   const [proceed, setProceed] = useState(false);
   const [failure, setFailure] = useState(false);
+  const passwordSave = (e) => {
+    setPassword(e.target.value);
+  };
+  const check = (e) => {
+    schema
+      .is()
+      .min(8)
+      .has()
+      .uppercase()
+      .has()
+      .lowercase()
+      .has()
+      .digits(1)
+      .has(/[!@#$%^&*()_+\-={};':"|,.<>?]/);
+    let res = schema.validate(e.target.value, { list: true });
+    // console.log(res);
+    setMa(true);
+    setSl(true);
+    setNa(true);
+    setSc(true);
+    setLength(true);
+    if (res.length !== 0) {
+      res.forEach((e) => {
+        if (e === "min") {
+          setLength(false);
+        }
+        if (e === "uppercase") {
+          setMa(false);
+        }
+        if (e === "lowercase") {
+          setSl(false);
+        }
+        if (e === "digits") {
+          setNa(false);
+        }
+        if (e === "has") {
+          setSc(false);
+        }
+      });
+    }
+
+    setNewpassword(e.target.value);
+  };
+  const check1 = (e) => {
+    setConfirmnewpassword(e.target.value);
+  };
 
   const submitForm = (e) => {
     e.preventDefault();
 
     const requestPassword = {
-      // password: "password",
-      // newpassword: "newpassword",
-      // confirmnewpassword:"confirmnewpassword",
-
-      password: "password",
-      new_password: "newpassword",
-      user_code: "userCode",
-      org_code: "orgCode",
+      password: password,
+      new_password: newpassword,
+      confirm_newpassword: confirmnewpassword,
+      user_code: "alokSTLIND",
+      org_code: "STLIND",
     };
-    // console.log(requestPassword);
+    console.log(requestPassword);
 
     axios({
       url: "https://liveexam.edusols.com/api/tassess_api.php?oper=PASSWORD_CHANGE",
@@ -50,6 +105,7 @@ export default function App() {
           const data = result.data;
           if (data.dbStatus === "SUCCESS") {
             setTimeout(() => {
+              setValidate(false);
               setProceed(true);
             }, 3000);
           } else if (data.dbStatus === "ERROR") {
@@ -75,7 +131,7 @@ export default function App() {
               sm={3}
               sx={{
                 backgroundImage:
-                  "url(https://wallpaperaccess.com/full/1657789.jpg)",
+                  `url(${back1})`,
                 backgroundRepeat: "no-repeat",
                 maxHeight: "100vh",
                 backgroundSize: "cover",
@@ -87,7 +143,7 @@ export default function App() {
                 {validate ? (
                   <Grid
                     item
-                    lg={8}
+                    lg={9}
                     sm={4}
                     mt={10}
                     sx={{ backgroundColor: "whitesmoke" }}
@@ -101,64 +157,141 @@ export default function App() {
                       <b>Change Default Password</b>
                       <Typography
                         variant="h11"
-                        sx={{ flexGrow: 1, mb: 2, ml: 27 }}
+                        sx={{ flexGrow: 1, mb: 2, ml: 45 }}
                       >
-                        <b>Welcome, Alok Sahoo</b>
+                        <b>Welcome, Shivangi Sahu</b>
                         <Button variant="outlined" size="small" color="warning">
                           <LogoutIcon /> LOGOUT
                         </Button>
                       </Typography>
                     </Typography>
-                    <Grid lg={8} sm={2}>
-                      <card sx={{ maxHeight: "100%", maxWidth: "80%" }}>
-                        <Box m={3}>
-                          <TextField
-                            label="Password"
-                            variant="outlined"
-                            type="text"
-                            name="password"
-                            margin="normal"
-                            onChange={(e) => setPassword(e.target.value)}
-                            fullWidth
-                          />
-                          <TextField
-                            autoComplete="newpassword"
-                            label=" New Password"
-                            variant="outlined"
-                            type="password"
-                            name="newpassword"
-                            margin="normal"
-                            onChange={(e) => setNewpassword(e.target.value)}
-                            fullWidth
-                          />
+                    <Grid container lg={10}>
+                      <Grid item lg={8} sm={2}>
+                        <card sx={{ maxHeight: "100%", maxWidth: "80%" }}>
+                          <Box m={3}>
+                            <TextField
+                              label="Password"
+                              variant="outlined"
+                              type="text"
+                              name="password"
+                              margin="normal"
+                              onChange={(e) => passwordSave(e)}
+                              fullWidth
+                            />
+                            <TextField
+                              autoComplete="newpassword"
+                              label=" New Password"
+                              variant="outlined"
+                              type="password"
+                              name="newpassword"
+                              margin="normal"
+                              onChange={(e) => check(e)}
+                              fullWidth
+                            />
 
-                          <TextField
-                            label=" Confirm New Password"
-                            variant="outlined"
-                            type="password"
-                            name="confirmnewpassword"
-                            margin="normal"
-                            onChange={(e) =>
-                              setConfirmnewpassword(e.target.value)
-                            }
-                            fullWidth
-                          />
+                            <TextField
+                              label=" Confirm New Password"
+                              variant="outlined"
+                              type="password"
+                              name="confirmnewpassword"
+                              margin="normal"
+                              onChange={(e) => check1(e)}
+                              fullWidth
+                            />
 
-                          <Button
-                            variant="contained"
-                            size="medium"
-                            color="success"
-                            endIcon={<ChangeCircleIcon />}
-                            fullWidth
-                            mt="2"
-                            type="submit"
-                            margin="normal"
-                            onClick={submitForm}
-                          >
-                            change
-                          </Button>
+                            <Button
+                              variant="contained"
+                              size="medium"
+                              color="success"
+                              endIcon={<ChangeCircleIcon />}
+                              fullWidth
+                              mt="2"
+                              type="submit"
+                              margin="normal"
+                              onClick={submitForm}
+                            >
+                              change
+                            </Button>
+                          </Box>
+                        </card>
+                      </Grid>
+                      <Grid item lg={2} sm={1}>
+                        <Box
+                          sx={{
+                            width: 250,
+                            height: 320,
+                            backgroundColor: "#b3e0f2",
+                            "&:hover": {
+                              backgroundColor: "primary.info",
+                              opacity: [0.9, 0.8, 0.7],
+                            },
+                            mb: 3,
+                            ml: 4,
+                          }}
+                        >
+                          <Typography variant="h5">
+                            <ReportIcon fontSize="medium" color="primary" />
+                            New Password
+                          </Typography>
+
+                          <hr />
+                          {ma ? (
+                            <p>
+                              <CheckCircleIcon style={{ color: "green" }} />
+                              Must Have Atleast One Capital Letter
+                            </p>
+                          ) : (
+                            <p>
+                              <CancelIcon style={{ color: "red" }} />
+                              Must Have Atleast One Capital Letter
+                            </p>
+                          )}
+                          {sl ? (
+                            <p>
+                              <CheckCircleIcon style={{ color: "green" }} />
+                              Must Have Atleast One Small Letter
+                            </p>
+                          ) : (
+                            <p>
+                              <CancelIcon style={{ color: "red" }} />
+                              Must Have Atleast One Small Letter
+                            </p>
+                          )}
+                          {Na ? (
+                            <p>
+                              <CheckCircleIcon style={{ color: "green" }} />
+                              Must Have Atleast One Number
+                            </p>
+                          ) : (
+                            <p>
+                              <CancelIcon style={{ color: "red" }} />
+                              Must Have Atleast One number
+                            </p>
+                          )}
+                          {Sc ? (
+                            <p>
+                              <CheckCircleIcon style={{ color: "green" }} />
+                              Must Have Atleast One Special Letter
+                            </p>
+                          ) : (
+                            <p>
+                              <CancelIcon style={{ color: "red" }} />
+                              Must Have Atleast One Special Letter
+                            </p>
+                          )}
+                          {length ? (
+                            <p>
+                              <CheckCircleIcon style={{ color: "green" }} />
+                              Should Be Minimum 8 Character
+                            </p>
+                          ) : (
+                            <p>
+                              <CancelIcon style={{ color: "red" }} />
+                              Should Be Minimum 8 Character
+                            </p>
+                          )}
                         </Box>
-                      </card>
+                      </Grid>
                     </Grid>
                   </Grid>
                 ) : (
@@ -178,7 +311,42 @@ export default function App() {
                         <Typography variant="h6">
                           <b>Password Changed failed....❌</b>
                         </Typography>
-                        <img src={warning} />
+                        <img src={warning} alt=""/>
+                      </Grid>
+                    </Grid>
+                  </Card>
+                ) : (
+                  ""
+                )}
+                {proceed ? (
+                  <Card
+                    sx={{
+                      marginTop: "80px",
+                      width: "710px",
+                      height: "300px",
+                      marginBottom: "161px",
+                    }}
+                  >
+                    <Grid container justifyContent="center">
+                      <Grid item lg={6} sm={3}>
+                        <CheckCircleOutlineIcon
+                          style={{
+                            color: "green",
+                            height: "120px",
+                            width: "200px",
+                            marginLeft:"60px",
+                            marginTop:"5px"
+                          }}
+                        />
+                        
+                        <Typography
+                          variant="h5"
+                          style={{
+                           color:"grey"
+                          }}
+                        >
+                          <b> Password Changed Done..</b>
+                        </Typography>
                       </Grid>
                     </Grid>
                   </Card>
@@ -187,13 +355,6 @@ export default function App() {
                 )}
               </Grid>
             </Grid>
-            {proceed ? (
-              <div>
-                <h1>Succesfully Password Changed</h1>
-              </div>
-            ) : (
-              ""
-            )}
           </Grid>
         </Box>
       </form>
